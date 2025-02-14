7d2aa455ffe0bc61588aada9ba0055c78120a7f1
Egor Chernakov <chernakov.eg@gmail.com>
2025-02-14 22:25:09 +0300

    repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,12 +9,13 @@ import (
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	command = command[:len(command)-1]
-	if err != nil {
-		fmt.Printf("error reading input: %v\n", err)
-		os.Exit(1)
+	for {
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		command = command[:len(command)-1]
+		if err != nil {
+			fmt.Printf("error reading input: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s: command not found\n", command)
 	}
-	fmt.Printf("%s: command not found\n", command)
 }
